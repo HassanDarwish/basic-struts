d104ff089dc78b1b5cfa7cb32f0d73ba382a92f8
Hassan.Ali <hassan.ali@intercome.com>
2023-07-17 16:58:13 +0200

    fix setting again

---
+++ basic_struts/src/main/java/basic_struts/model/MessageStore.java
@@ -0,0 +1,16 @@
+package basic_struts.model;
+
+public class MessageStore {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
